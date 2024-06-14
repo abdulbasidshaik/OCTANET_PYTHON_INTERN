c9c22dc9c9623997e9576b25ba6e9fccb90d9eb4
abdulbasidshaik <abdulbasidshaik4448@gmail.com>
2024-06-14 16:13:12 +0530

    first commit

---
+++ TASK-1/task1_atm_interface.py
@@ -0,0 +1,18 @@
+ATM INTERFACE 
+
+logo = """
+
+   __   ____  __  __    __  __    __    ___  _   _  ____  _  _  ____ 
+  /__\ (_  _)(  \/  )  (  \/  )  /__\  / __)( )_( )(_  _)( \( )( ___)
+ /(__)\  )(   )    (    )    (  /(__)\( (__  ) _ (  _)(_  )  (  )__) 
+(__)(__)(__) (_/\/\_)  (_/\/\_)(__)(__)\___)(_) (_)(____)(_)\_)(____)
+
+"""
+
+The ATMs in our cities are built on Python, as we have all seen them. It is a console-based application with five different classes. In order to use the system, the user must enter his or her user ID and pin when it starts. Once the details are entered successfully, ATM functionality is unlocked. As a result of the project, the following operations can be performed:
+
+- Transactions History
+- Withdraw
+- Deposit
+- Transfer
+- Quit
